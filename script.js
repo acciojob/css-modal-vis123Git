fc911ab2ec0bc558ff02d8f5ec79772dd48536ca
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-07-14 22:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const button = document.getElementById("openModal")
 const modal = document.getElementsByClassName("modal")
 
 function openModalFunction(params) {
-	console.log("modal=",modal[0])
 	modal[0].style.visibility = "visible";
 }
 
 function closeModalFunction(params) {
-	modal[0].style.visibility = "hidden";
+	modal[0].style.display = "none";
 }
